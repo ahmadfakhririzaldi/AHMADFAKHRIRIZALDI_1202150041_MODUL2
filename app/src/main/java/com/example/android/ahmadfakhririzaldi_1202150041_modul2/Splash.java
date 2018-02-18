229fb34ab2a613cc661ca4591af4e70b6df8e70d
unknown <ahmadfakhririzaldy@gmail.com>
2018-02-18 23:00:01 +0700

    AHMADFAKHRIRIZALDI_1202150041_MODUL2

---
+++ app/src/main/java/com/example/android/ahmadfakhririzaldi_1202150041_modul2/Splash.java
@@ -0,0 +1,25 @@
+package com.example.android.ahmadfakhririzaldi_1202150041_modul2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Toast.makeText(this,"AHMAD FAKHRI RIZALDI_1202150041",Toast.LENGTH_SHORT).show();
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 3000L); //3000 L = 3 detik
+    }
+}
